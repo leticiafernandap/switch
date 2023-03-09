0b284f76c3a63d052e589cc5a142ba80adb387f3
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-03-09 15:43:53 -0300

    swith

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    $menu = -1;
+    switch($menu){
+        case 1:
+        case 2:
+            echo "Administração";
+            echo "<br/>";
+        break;
+        case 3:
+            echo "Biomedicina";
+            echo "<br/>";
+        break;
+        case 4:
+            echo "Design Gráfico";
+            echo "<br/>";
+        break;
+         case 5:
+            echo "Física";
+            echo "<br/>";
+        break;
+        case 6:
+            echo "Jornalismo";
+            echo "<br/>";
+        break;
+        case 7:
+        case 8:
+        case 9:
+            echo "Medicina";
+            echo "<br/>";
+        break;
+        case 10:
+         echo "Relações Públicas";
+         echo "<br/>";
+        break;
+        default:
+        echo "código inválido";
+        break;
+
+
+
+
+    }
+?>
